0a8cac965842fbe01c2b52481753b6490a70ce18
babooLoggins <mbarlowvt@gmail.com>
2018-01-01 16:26:48 -0500

    Adds the rest of the bubble sort tests

--- src/test/java/sorting/BubbleSortTests.java
+++ src/test/java/sorting/BubbleSortTests.java
@@ -19,4 +19,52 @@ public class BubbleSortTests {
         }
     }
 
+    @Test
+    public void testDoubleBubbleSort(){
+        Set<double[]> doubleTestSet = TestData.getDoubleTestData();
+        int i = 0;
+        for(double[] testSet : doubleTestSet){
+            BubbleSort.sort(testSet);
+            i++;
+            System.out.println("Testing set " + i);
+            assert(SortingChecker.isSortedDoubleArray(testSet));
+        }
+    }
+
+    @Test
+    public void testLongBubbleSort(){
+        Set<long[]> doubleTestSet = TestData.getLongTestData();
+        int i = 0;
+        for(long[] testSet : doubleTestSet){
+            BubbleSort.sort(testSet);
+            i++;
+            System.out.println("Testing set " + i);
+            assert(SortingChecker.isSortedLongArray(testSet));
+        }
+    }
+
+    @Test
+    public void testFloatBubbleSort(){
+        Set<float[]> doubleTestSet = TestData.getFloatTestData();
+        int i = 0;
+        for(float[] testSet : doubleTestSet){
+            BubbleSort.sort(testSet);
+            i++;
+            System.out.println("Testing set " + i);
+            assert(SortingChecker.isSortedFloatArray(testSet));
+        }
+    }
+
+    @Test
+    public void testShortBubbleSort(){
+        Set<short[]> doubleTestSet = TestData.getShortTestData();
+        int i = 0;
+        for(short[] testSet : doubleTestSet){
+            BubbleSort.sort(testSet);
+            i++;
+            System.out.println("Testing set " + i);
+            assert(SortingChecker.isSortedShortArray(testSet));
+        }
+    }
+
 }
