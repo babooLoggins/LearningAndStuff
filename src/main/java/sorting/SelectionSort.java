3319a825650730739c264e3497bcb0d79df19f53
babooLoggins <mbarlowvt@gmail.com>
2018-01-02 21:09:43 -0500

    Reorders methods alphabetically

--- src/main/java/sorting/SelectionSort.java
+++ src/main/java/sorting/SelectionSort.java
@@ -2,15 +2,15 @@ package sorting;
 
 public class SelectionSort {
 
-    public static void sort(int[] input){
-
-    }
-
     public static void sort(double[] input){
 
     }
 
     public static void sort(float[] input){
+
+    }
+
+    public static void sort(int[] input){
 
     }
 
