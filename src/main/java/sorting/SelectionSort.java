5a47ab72bb79c631cccd94329a60d112aa317a62
babooLoggins <mbarlowvt@gmail.com>
2018-01-02 21:07:45 -0500

    Creates initial SelectionSort class and sets up method headers that will need to be filled out

---
+++ src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package sorting;
+
+public class SelectionSort {
+
+    public static void sort(int[] input){
+
+    }
+
+    public static void sort(double[] input){
+
+    }
+
+    public static void sort(float[] input){
+
+    }
+
+    public static void sort(long[] input){
+
+    }
+
+    public static void sort(short[] input){
+
+    }
+}
