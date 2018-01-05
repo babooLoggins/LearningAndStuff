bf737be630351a4423a28f26e18137e8e8fd5102
babooLoggins <mbarlowvt@gmail.com>
2018-01-05 18:21:08 -0500

    Initial commit of methods and unit tests to validate them

---
+++ src/main/java/sorting/QuickSort.java
@@ -0,0 +1,31 @@
+package sorting;
+
+public class QuickSort {
+    public static void sort(double[] input){
+
+    }
+
+    public static void sort(float[] input){
+
+    }
+
+    public static void sort(int[] input){
+
+    }
+
+    public static void sort(long[] input){
+
+    }
+
+    public static void sort(short[] input){
+
+    }
+
+    public static void main(String[] args){
+        final double[] doubleArr1 = new double[]{1.3, 4.2, 6.4, 2.3, 3.1, 7.4, 7.1};
+        sort(doubleArr1);
+        for(double d : doubleArr1){
+            System.out.println(d);
+        }
+    }
+}
