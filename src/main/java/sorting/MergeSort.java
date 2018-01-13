7e7159e729d2610e8ed5b371c5d89dbfa802784a
Mike Barlow <mbarlowvt@gmail.com>
2018-01-12 21:53:38 -0500

    Sets up the base methods to be implemented and the tests to confirm their functionality

---
+++ src/main/java/sorting/MergeSort.java
@@ -0,0 +1,23 @@
+package sorting;
+
+public class MergeSort {
+    public static void sort(int[] input){
+
+    }
+
+    public static void sort(double[] input){
+
+    }
+
+    public static void sort(float[] input){
+
+    }
+
+    public static void sort(long[] input){
+
+    }
+
+    public static void sort(short[] input){
+
+    }
+}
