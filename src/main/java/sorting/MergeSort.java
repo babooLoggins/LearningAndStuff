c55c76d94d54b35eeb8eb1804c0f41aded87eee3
Mike Barlow <mbarlowvt@gmail.com>
2018-01-14 21:19:20 -0500

    Preliminary MergeSort implementation for int arrays

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -1,8 +1,44 @@
 package sorting;
 
-public class MergeSort {
-    public static void sort(int[] input){
+import java.util.Arrays;
 
+public class MergeSort {
+
+    static int[] merge(int[] lArr, int[] rArr){
+        int totalLength = lArr.length+rArr.length;
+        int[] output = new int[totalLength];
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        while(leftIndex + rightIndex < totalLength){
+            if(leftIndex < lArr.length && rightIndex < rArr.length){
+                if(lArr[leftIndex] <= rArr[rightIndex]){
+                    output[leftIndex + rightIndex] = lArr[leftIndex];
+                    leftIndex++;
+                } else {
+                    output[leftIndex + rightIndex] = rArr[rightIndex];
+                    rightIndex++;
+                }
+            } else if(leftIndex < lArr.length){
+                output[leftIndex + rightIndex] = lArr[leftIndex];
+                leftIndex++;
+            } else {
+                output[leftIndex + rightIndex] = rArr[rightIndex];
+                rightIndex++;
+            }
+
+        }
+        return output;
+    }
+
+    static int[] mergesort(int[] array){
+        if(array.length > 1){
+            int m = array.length / 2;
+            int[] leftArray = mergesort(Arrays.copyOfRange(array, 0, m));
+            int[] rightArray = mergesort(Arrays.copyOfRange(array, m, array.length));
+            array = merge(leftArray, rightArray);
+        }
+        return array;
     }
 
     public static void sort(double[] input){
@@ -19,5 +55,13 @@ public class MergeSort {
 
     public static void sort(short[] input){
 
+    }
+
+    public static void main(String[] args){
+        int[] intArr1 = new int[]{1, 4, 6, 2, 3, 7, 5};
+        int[] sortedArray = mergesort(intArr1);
+        for(int val : sortedArray){
+            System.out.println(val);
+        }
     }
 }
